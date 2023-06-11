710013dc0bec76e3ae85658892af095a98d0cdb7
onisjiety <onisjiety@gmail.com>
2023-06-11 12:13:16 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,10 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
  * main - print numbers of augument passed into it
  * @argc: augument count
- * @*argv[]: augument vector
+ * @argv: augument vector
  * Return: always return 0 on success
  */
 
