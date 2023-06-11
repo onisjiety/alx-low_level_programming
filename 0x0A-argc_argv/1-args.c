6c47b44470c340c9c0f8ab816362a463d53a55e5
onisjiety <onisjiety@gmail.com>
2023-06-11 12:38:27 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,14 @@
  * main - print numbers of augument passed into it
  * @argc: augument count
  * @argv: augument vector
- * Return: always return 0 on success
+ * Return: always return 0 on 
  */
 
 int main(int argc, char *argv[])
 {
-    printf("%d\n", argc - 1); 
-    
-    return 0;
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	
+	return 0;
 }
