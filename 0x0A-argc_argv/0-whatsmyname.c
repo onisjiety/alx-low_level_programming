781991222302b763f642b50964515bd000722f57
onisjiety <onisjiety@gmail.com>
2023-06-11 11:31:04 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - Print name of index and change index name
+ * @argc: augument c
+ * @*argv[]: augument V
+ * Return: always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i;
+    
+	for (i = 0; i < argc; i++)
+       	{
+	printf("%s\n", argv[i]); 
+	}
+
+	return 0;
+}
