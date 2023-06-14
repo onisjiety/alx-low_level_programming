63162a80f4ca1683603cfd466d8398843b8224c5
onisjiety <onisjiety@gmail.com>
2023-06-14 10:07:28 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - array of character , initialized with character
+ * @size:
+ * @c:
+ * Return: always 0 on success
+ */
+
+char* create_array(unsigned int size, char c) {
+    
+	unsigned int i;
+	char* array;
+
+    if (size == 0) {
+        return NULL;
+    }
+
+    array = (char*)malloc(size * sizeof(char));
+    if (array == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < size; i++) {
+        array[i] = c;
+    }
+
+    return array;
+}
