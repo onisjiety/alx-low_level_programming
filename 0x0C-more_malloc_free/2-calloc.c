56387a819764f59dd0b6ea414ecd718c47278299
onisjiety <onisjiety@gmail.com>
2023-06-16 15:52:39 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size) {
+    void *ptr;
+
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    ptr = malloc(nmemb * size);
+
+    if (ptr == NULL)
+        return NULL;
+
+    memset(ptr, 0, nmemb * size);
+
+    return ptr;
+}
