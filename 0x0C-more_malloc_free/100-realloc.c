9d9715c0aec3f11e485923f6ad68a3a01e975dbd
onisjiety <onisjiety@gmail.com>
2023-06-16 16:37:02 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *new_ptr;
+    unsigned int min_size;
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    if (ptr == NULL)
+    {
+        return malloc(new_size);
+    }
+
+    if (new_size == old_size)
+    {
+        return ptr;
+    }
+
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+    {
+        return NULL;
+    }
+
+    min_size = old_size < new_size ? old_size : new_size;
+    memcpy(new_ptr, ptr, min_size);
+    free(ptr);
+
+    return new_ptr;
+}
+
