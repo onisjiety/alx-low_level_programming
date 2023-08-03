18e884af6618f6f2b0d1aeb270bbfa8a2ac6728a
root <onisjiety@gmail.com>
2023-08-03 15:51:07 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,14 @@
  */
 
 int factorial(int n) {
-    if (n < 0) {
-        return -1; // Return -1 to indicate an error for negative inputs
+    	int i;
+	if (n < 0) {
+        return -1;
     } else if (n == 0) {
-        return 1; // Factorial of 0 is 1
+        return 1;
     } else {
         int result = 1;
-        for (int i = 1; i <= n; i++) {
+        for (i = 1; i <= n; i++) {
             result *= i;
         }
         return result;
