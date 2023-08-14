7b5eab554f19f2a7a1d2cba4d46faf7b346bbfb6
root <onisjiety@gmail.com>
2023-08-14 05:48:01 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner) {
-   strcpy(d->name, name);
+    strcpy(d->name, name);
     d->age = age;
-    strcpy(d->owner,owner);
+    strcpy(d->owner, owner);
 }
