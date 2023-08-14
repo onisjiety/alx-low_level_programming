77044e9e5768916d2f6c77bc4b60e51ac2c888c4
root <onisjiety@gmail.com>
2023-08-14 05:19:45 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,14 @@ struct dog
 
 /**
  * dog_t - typedef for struct dog
+ * init_dog - for struct
+ * @d: character struct
+ * @name: character pointer
+ * @age: float
+ * @owner: character pointer
+ * print_dog - struct pointer
+ * @d: varieble name;
+ * *new_dog - struct
  */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
