a5fc16601d6b66ab42b718e04f4947a473678dec
root <onisjiety@gmail.com>
2023-08-30 13:20:14 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,13 +6,15 @@
  * Return: always 0 on succes
  */
 
-void _print_rev_recursion(char *s) {
-
-
-    if (*s == '\0') {
-        return;
-    } else {
-        _print_rev_recursion(s + 1);
-        putchar(*s);
-    }
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+	else
+	{
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+	}
 }
