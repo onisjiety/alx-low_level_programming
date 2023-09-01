022001bcfd6cbc03f6474489597fa378630e7f07
root <onisjiety@gmail.com>
2023-09-01 16:10:11 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,37 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int cents;
-    int numCoins = 0;
+/**
+ * main -  program that prints the minimum number of money
+ * @argc: augument count
+ * @argv: augument vector
+ * Return: always on success
+ */
 
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+int main(int argc, char *argv[])
+{
+	int cents;
+	int numCoins = 0;
 
-    cents = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
-    }
+	cents = atoi(argv[1]);
 
-    numCoins += cents / 25;
-    cents %= 25;
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    numCoins += cents / 10;
-    cents %= 10;
+	numCoins += cents / 25;
+	cents %= 25;
 
-    numCoins += cents / 5;
-    cents %= 5;
+	numCoins += cents / 10;
+	cents %= 10;
 
-    numCoins += cents / 2;
-    cents %= 2;
+	numCoins += cents / 5;
+	cents %= 5;
 
-    numCoins += cents;
+	numCoins += cents / 2;
+	cents %= 2;
 
-    printf("%d\n", numCoins);
-    return 0;
+	numCoins += cents;
+
+	printf("%d\n", numCoins);
+	return (0);
 }
 
