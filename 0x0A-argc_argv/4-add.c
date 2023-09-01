9147f611d69cb181a7ef7df3b7d6ad642d60b917
root <onisjiety@gmail.com>
2023-09-01 15:44:38 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,27 +3,38 @@
 #include <ctype.h>
 #include "main.h"
 
-int main(int argc, char *argv[]) {
-    int result = 0;
-    int i;
+/**
+ * main -  program that adds positive numbers.
+ * @argc: augument count
+ * @argv: augument vector
+ * Return: always on success
+ */
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
 
-    for (i = 1; i < argc; i++) {
-        int j;
-        for (j = 0; argv[i][j] != '\0'; j++) {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-        }
-        result += atoi(argv[i]);
-    }
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (0);
+	}
 
-    printf("%d\n", result);
-    return 0;
+	for (i = 1; i < argc; i++)
+	{
+
+		int j;
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+	}
+	result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
 }
-
