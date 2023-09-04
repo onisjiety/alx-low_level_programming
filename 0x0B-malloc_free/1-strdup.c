1be660daea826eb614bdbd82dbfba732618ad9be
root <onisjiety@gmail.com>
2023-09-04 13:02:48 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,25 +5,27 @@
 
 /**
  * _strdup - memory that contains a copy of a string
- * @str:
+ * @str: character pointer
  * Return: always 0 on success
  */
-char* _strdup(char* str) {
-    size_t len;
-    char* duplicate;
 
-    if (str == NULL) {
-        return NULL;
-    }
+char *_strdup(char *str)
+{
+	size_t len;
+	char *duplicate;
 
-    len = strlen(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-    duplicate = (char*)malloc((len + 1) * sizeof(char));
-    if (duplicate == NULL) {
-        return NULL;
-    }
+	len = strlen(str);
+	duplicate = (char *)malloc((len + 1) * sizeof(char));
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
 
-    strcpy(duplicate, str);
-
-    return duplicate;
+	strcpy(duplicate, str);
+	return (duplicate);
 }
